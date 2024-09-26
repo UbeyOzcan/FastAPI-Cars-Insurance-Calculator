76f7646d66ba0a8135861f48879559cd008650a1
Ubeydullah Ozcan <40140140+UbeyOzcan@users.noreply.github.com>
2024-09-26 12:48:09 +0200

    Delete Datasets directory

--- Datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-
-def get_data() -> pd.DataFrame:
-    path = os.path.dirname(__file__)
-    df = pd.read_csv(os.path.join(path, "frenchmtpl_clean.csv"), sep=';')
-    return df
